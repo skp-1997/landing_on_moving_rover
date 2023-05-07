bd92b3be68b3fe8efb72870826f83756e15203df
skp-1997 <skpatil.ai@gmail.com>
2023-05-07 22:47:01 +0000

    Suggested new changes in visual servoing

--- scripts/autonomous_landing.py
+++ scripts/autonomous_landing.py
@@ -25,10 +25,8 @@ class Mission:
         self.tag_pt = None
         self.tag_ori = None
         self.tranformation_mat = np.matrix([[1, 0], [0, -1]])
-        self.vel = np.matrix([[0],[0]])
+        self.vel = np.matrix([[0], [0]])
         self.err = np.matrix([[0], [0]])
-
-
 
         self.KP = 0.01
         self.KD = 0.0000
@@ -75,14 +73,11 @@ class Mission:
             print("Check 2")
             self.rate.sleep()
 
-        # if set_mode_client.call(offb_set_mode).mode_sent == True:
-        #     self.offboardCheck = True
-        #     rospy.loginfo("OFFBOARD enabled")
+
 
         last_req = rospy.Time.now()
         while not rospy.is_shutdown():  # and not self.offboardCheck:
-            # print("Inside while loop :")
-            # print(rospy.Time.now(), "time_now and :    ", last_req )
+
             if self.drone_status.mode != "OFFBOARD" and (rospy.Time.now() - last_req) > rospy.Duration(2.0):
                 print("Check 3")
                 if set_mode_client.call(offb_set_mode).mode_sent:
@@ -124,7 +119,7 @@ class Mission:
                 azimuth -= 2.0 * math.pi
             else:
                 azimuth += 2.0 * math.pi
-            q = quaternion_from_euler(0, 0, 3.14/2)
+            q = quaternion_from_euler(0, 0, 3.14 / 2)
             # print(q)
             self.goal_pose.pose.orientation.x = q[0]
             self.goal_pose.pose.orientation.y = q[1]
@@ -140,7 +135,7 @@ class Mission:
 
     def goto_rover(self):
         print("=== GO to rock and Face rock ========")
-        locations = np.matrix([[self.rovr_pose.x, self.rovr_pose.y + 2, 7.0],])
+        locations = np.matrix([[self.rovr_pose.x, self.rovr_pose.y + 2, 7.0], ])
         # locations = np.matrix([[-1.0, 0.0, 7.0], ])
         for waypt in locations:
             x, y, z = waypt.tolist()[0]
@@ -157,7 +152,6 @@ class Mission:
         des_vel.velocity.z = z
         return des_vel
 
-        
     def descent(self):
         rate = rospy.Rate(40)
         self.x_change = 1
@@ -172,68 +166,27 @@ class Mission:
 
         while not rospy.is_shutdown():
 
-            
             if self.tag_pt is not None:
                 print("Keeping in center")
 
                 err_x = self.tag_pt.x
                 err_y = self.tag_pt.y
                 err_z = self.tag_pt.z
-                # x_ori = self.tag_ori.x
-                # y_ori = self.tag_ori.y
-                # z_ori = self.tag_ori.z
-                # w_ori = self.tag_ori.w
-                #
-                # e = euler_from_quaternion([x_ori, y_ori, z_ori, w_ori])
-                #
-                # q = quaternion_from_euler(0, 0, e[2])
-                #
-                # cmd_ori = PoseStamped()
-                # cmd_ori.pose.orientation.x = q[0]
-                # cmd_ori.pose.orientation.y = q[1]
-                # cmd_ori.pose.orientation.z = q[2]
-                # cmd_ori.pose.orientation.w = q[3]
 
+                self.x_change += err_x * self.KP + (self.x_prev_error * self.KD) + (
+                        self.x_sum_error * self.KI)  # chnage := change + err
+                self.y_change += err_y * self.KP + (self.y_prev_error * self.KD) + (self.y_sum_error * self.KI)
 
-
-
-                '''
-                self.x_change += abs(err_x * self.KP + (self.x_prev_error * self.KD) + (self.x_sum_error * self.KI))
-                self.y_change += abs(err_y * self.KP + (self.y_prev_error * self.KD) + (self.y_sum_error * self.KI))
-
-                self.x_change = min(pid_clip * err_z / dcnt_h, self.x_change)
-                self.y_change = min(pid_clip * err_z / dcnt_h, self.y_change)
+                self.x_change = max(min(pid_clip * err_z / dcnt_h, self.x_change), -pid_clip * err_z / dcnt_h)
+                self.y_change = max(min(pid_clip * err_z / dcnt_h, self.y_change), -pid_clip * err_z / dcnt_h)
 
                 self.err[0][0] = self.x_change
                 self.err[1][0] = self.y_change
 
                 self.vel = np.matmul(self.tranformation_mat, self.err)
-                '''
 
-                self.x_change += abs(err_x * self.KP  + (self.x_prev_error * self.KD) + (self.x_sum_error * self.KI))
-                self.y_change += abs(err_y * self.KP  + (self.y_prev_error * self.KD) + (self.y_sum_error * self.KI))
-            
-                self.x_change = min(pid_clip*err_z/dcnt_h, self.x_change)
-                self.y_change = min(pid_clip*err_z/dcnt_h, self.y_change)
-                
+                des = self.get_descent(self.vel[0][0], self.vel[1][0], -0.3)
 
-                # print(self.x_change, self.y_change)
-
-                if err_x > 0 and err_y < 0:
-                    print('Q4')
-                    des = self.get_descent(self.x_change, self.y_change, -0.3)
-                elif err_x < 0 and err_y > 0:
-                    print('Q2')
-                    des = self.get_descent(-self.x_change, -self.y_change, -0.3)
-                elif err_x > 0 and err_y > 0:
-                    print('Q1')
-                    des = self.get_descent(self.x_change, -self.y_change, -0.3)
-                elif err_x < 0 and err_y < 0:
-                    print('Q3')
-                    des = self.get_descent(-self.x_change, self.y_change, -0.3)
-
-                # des = self.get_descent(self.vel[0][0], self.vel[1][0], -0.3)
-                # self.local_pose_pub.publish(cmd_ori)
                 self.vel_pub.publish(des)
                 timeout -= 1
                 rate.sleep()
@@ -242,10 +195,8 @@ class Mission:
                 self.x_sum_error += err_x
                 self.y_sum_error += err_y
 
-
                 if timeout == 0 and self.curr.z > 0.7:
                     timeout = 30
-                    print timeout
                     self.x_change = 0
                     self.y_change = 0
                     self.x_sum_error = 0
@@ -255,27 +206,12 @@ class Mission:
 
                 if self.curr.z < 2.0:
                     print("landing")
-
-                    # des=self.get_descent(0, 0.5, -0.3)
-                    # self.vel_pub.publish(des)
                     self.land_on_rover()
                     break
 
-            # else:
-                
-            #     self.follow_rover()
-            #     if self.curr.z < 2.5:
-            #         print("landing")
-            #         # des=self.get_descent(0, 0.5, -0.3)
-            #         # self.vel_pub.publish(des)
-            #         self.land_on_rover()
-
-                
-                  
         print('Exiting while in decent')
 
-    def follow_rover(self):
-        print('Cheating')
+    def follow_rover(self)
         self.correct_pose.pose.position.x = self.rovr_pose.x
         self.correct_pose.pose.position.y = self.rovr_pose.y
         self.correct_pose.pose.position.z = self.curr.z
@@ -287,7 +223,7 @@ class Mission:
         local_pos_pub = rospy.Publisher('/uav1/mavros/setpoint_raw/local', PositionTarget, queue_size=10)
         arm_service = rospy.ServiceProxy('/uav1/mavros/cmd/arming', CommandBool)
         landing_position = PositionTarget()
-        landing_position.position.x = self.curr.x # self.rovr_pose.x
+        landing_position.position.x = self.curr.x  # self.rovr_pose.x
         landing_position.position.y = self.curr.y
         landing_position.position.z = -3.5
         landing_position.coordinate_frame = PositionTarget.FRAME_LOCAL_NED
@@ -302,30 +238,6 @@ class Mission:
             if self.curr.z < -3.2:
                 print("Landed =========")
                 arm_service(False)
-
-    
-    
-    
-    # def land_on_rover(self):
-    #     local_pos_pub = rospy.Publisher('/uav1/mavros/setpoint_raw/local', PositionTarget, queue_size=10)
-    #     arm_service = rospy.ServiceProxy('/uav1/mavros/cmd/arming', CommandBool)
-    #     landing_position = PositionTarget()
-    #     landing_position.position.x = 12.621
-    #     landing_position.position.y = -64.85
-    #     landing_position.position.z = -3.5
-    #     landing_position.coordinate_frame = PositionTarget.FRAME_LOCAL_NED
-    #     landing_position.type_mask = PositionTarget.IGNORE_VX + PositionTarget.IGNORE_VY + PositionTarget.IGNORE_VZ \
-    #                                  + PositionTarget.IGNORE_AFX + PositionTarget.IGNORE_AFY + PositionTarget.IGNORE_AFZ \
-    #                                  + PositionTarget.IGNORE_YAW + PositionTarget.IGNORE_YAW_RATE
-    #     # Publish the landing position command
-    #     rate = rospy.Rate(10)
-    #     while not rospy.is_shutdown():
-    #         local_pos_pub.publish(landing_position)
-    #         rate.sleep()
-    #         if (self.curr.z < -3.2):
-    #             print("Landed =========")
-    #             arm_service(False)
-
 
     def pose_cb(self, points):
         self.present_pose = points
